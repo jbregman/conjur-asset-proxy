da3ef26c95aee8577b1d7ad8733ddad9e1abc00f
jbregman <jbregman@gmail.com>
2016-02-17 20:55:59 +0000

    Modified policy to have layers

--- test-basic/test-policy.rb
+++ test-basic/test-policy.rb
@@ -1,6 +1,15 @@
 policy "test" do
 
-	variable "username"
-	variable "password"
+	username = variable "username"
+	password = variable "password"
 
+	layer "puppet" do
+
+		can 'read', username
+		can 'execute', username
+	
+		can 'read', password
+		can 'execute', password	
+
+	end 
 end
